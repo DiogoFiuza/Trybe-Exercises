25bf956b6632a0c319dbe2e8df196afc601f84e5
Diogo Fiuza <difipalm@gmail.com>
2021-06-24 11:42:23 -0300

    Exercício 6 e 7

--- Fundamentos/bloco_4/dia_1/script.js
+++ Fundamentos/bloco_4/dia_1/script.js
@@ -51,7 +51,47 @@ switch(valor > 0){
       console.log("zero");
 }
 // Exercício 5
-// 
+/*Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. */
+let angulo1 = 60;
+let angulo2 = 50;
+let angulo3 = 70;
+let somaDosAngulos = (angulo1+angulo2+angulo3);
+
+if(angulo1 <= 0 || angulo2 <= 0 || angulo3 <= 0){
+  console.log("Erro no valor do ângulo")
+}else{
+  if(somaDosAngulos != 180 || somaDosAngulos < 0){
+    console.log("false");
+  }else{
+    console.log("true")
+  }
+}
+// Exercício 6
+// Mostrar o movimento da peça de xadres indicada
+let pecaDeXadrex = "peão";
+
+switch(pecaDeXadrex){
+  case "rei":
+    console.log("O rei pode mover-se em todas as direções (horizontal, vertical e diagonal) somente uma casa de cada vez.");
+    break;
+  case "rainha":
+    console.log("A rainha move-se ao longo da horizontal, vertical e diagonais mas não pode pular outras peças.");
+    break;
+  case "bispo":
+    console.log("O bispo move-se ao longo da diagonal. Não pode pular outras peças.");
+    break;
+  case "cavalo":
+    console.log("É a única peça que pode pular as outras. O movimento do cavalo é em forma de “L”, quer dizer, duas casas em sentido horizontal e mais uma na vertical ou vice-versa.");
+    break;
+  case "torre";
+    console.log("A torre movimenta-se pela vertical ou horizontal, mas não pode pular outras peças.");
+    break;
+  case "peão"
+    console.log("O peão movimenta-se apenas uma casa para frente e somente captura outras peças na diagonal. Opcionalmente, cada peão pode avançar duas casas no seu primeiro movimento do jogo.");
+    break;
+  default:
+    onsole.log("Peça inválida!");
+}
 
 
 // Exercício 7
