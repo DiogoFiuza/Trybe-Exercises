c216f0a38ac0aab226725950b108559ea4fc1fdd
Diogo Fiuza <difipalm@gmail.com>
2021-06-24 15:03:50 -0300

    arrumando o código

--- Fundamentos/bloco_4/dia_1/script.js
+++ Fundamentos/bloco_4/dia_1/script.js
@@ -98,26 +98,26 @@ switch(pecaDeXadrex){
 // Converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F
 let porcentagem = 80;
 
-if(porcentagem >= 90 && porcentagem <100){
-  console.log("A"); 
-}else if(Porcentagem >= 80 ){
-  console.log("B");
-}else if(Porcentagem >= 70){
-  console.log("C");
-}else if(Porcentagem >= 60){
-  console.log("D");
-}else if(Porcentagem >= 50){
-  console.log("E");
-}else if(Porcentagem < 50 && porcentagem > 0){
-  console.log("F");
-}else{
+if (porcentagem > 100 || porcentagem < 0){
   console.log("ERRO");
+} else if(porcentagem >= 90 && porcentagem <100){
+    console.log("A"); 
+} else if (porcentagem >= 80 ){
+    console.log("B");
+} else if (porcentagem >= 70){
+    console.log("C");
+} else if (porcentagem >= 60){
+    console.log("D");
+} else if (porcentagem >= 50){
+    console.log("E");
+} else {
+    console.log("F");
 }
 
 // Exercício 8
 /*Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .*/
 
-let num1 = 1;
+let num1 =1;
 let num2 = 2;
 let num3 = 4;
 
@@ -129,11 +129,11 @@ if(num1 % 2 ==  0 || num2 % 2 == 0 || num3 % 3 == 0){
 // Exercício 9
 // Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
 
-let num1 = 1;
-let num2 = 2;
-let num3 = 4;
+let n1 = 1;
+let n2 = 2;
+let n3 = 4;
 
-if(num1 % 2 !=  0 || num2 % 2 != 0 || num3 % 3 != 0){
+if(n1 % 2 !=  0 || n2 % 2 != 0 || n3 % 3 != 0){
   console.log(true);
 }else {
   console.log(false);
