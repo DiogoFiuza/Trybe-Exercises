e848cb6fae597e7b4f9f9c31c97051847835bd14
Diogo Fiuza <difipalm@gmail.com>
2021-07-14 19:53:02 -0300

    Iniciando e entregando primeiros requisitos do forms

---
+++ Fundamentos/bloco_6/script.js
@@ -0,0 +1,12 @@
+const statesSelect = document.querySelector('.estados');
+const states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espirito Santo', 'Goiás', 'Maranhão', 'Mato Grosso do Sul', 'Mato Grosso', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+
+function createStates(){
+  for(let index in states){
+    const newState = document.createElement('option');
+    newState.innerText = states[index];
+    statesSelect.appendChild(newState);
+  }
+}
+createStates();
+
