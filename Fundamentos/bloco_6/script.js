269cc2ee53cfe876a6ed94560164b4a52f42841d
Diogo Fiuza <difipalm@gmail.com>
2021-07-15 20:57:57 -0300

    Aplicando Bootstrap

--- Fundamentos/bloco_6/script.js
+++ Fundamentos/bloco_6/script.js
@@ -1,5 +1,7 @@
 const statesSelect = document.querySelector('.estados');
 const states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espirito Santo', 'Goiás', 'Maranhão', 'Mato Grosso do Sul', 'Mato Grosso', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+const data = document.querySelector('#data');
+const cpf = document.querySelector('#cpf');
 
 function createStates(){
   for(let index in states){
