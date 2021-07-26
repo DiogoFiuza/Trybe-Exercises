a4ea01a0dfa86c6af81076de25c7947bfd7aee93
Diogo Fiuza <difipalm@gmail.com>
2021-07-26 11:40:42 -0300

    Exercício 2

--- Fundamentos/bloco_7/dia_1/Exercício2.js
+++ Fundamentos/bloco_7/dia_1/Exercício2.js
@@ -1,10 +1,27 @@
 // Parte 2
 // Exercício 1
-function fatorial(number){
+const fatorial = number => {
   let result = 1;
   for(let index = number; index > 0 ; index-= 1){
-    result = result * index;
+    result *= index;
   }
   return result;
 }
 console.log(fatorial(5));
+
+
+// Exxercício 2
+const biggerWord = phrase => {
+  let words = phrase.split(' ');
+  let sizeWord = 0;
+  let result = '';
+
+  for(let index of words){
+    if(index.length > sizeWord){
+      result = index;
+      sizeWord = index.length;
+    }
+  }return result;
+}
+
+console.log(biggerWord("Antônio foi no banheiro e não sabemos o que aconteceu"))
