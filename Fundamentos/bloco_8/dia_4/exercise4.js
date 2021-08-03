23b2c9f3723563400fd45e8d5a33ba8484c01aa0
Diogo Fiuza <difipalm@gmail.com>
2021-08-02 22:30:49 -0300

    Exercicio 4

---
+++ Fundamentos/bloco_8/dia_4/exercise4.js
@@ -0,0 +1,25 @@
+const books = require("./module");
+const assert = require('assert');
+
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+};
+
+function longestNamedBook() {
+  // escreva seu código aqui
+  return books.reduce((acc, cur) => {
+   if(acc.name.length < cur.name.length) {
+    return  cur;
+   }
+  return acc;
+  })
+}
+console.log(longestNamedBook())
+assert.deepStrictEqual(longestNamedBook(), expectedResult);
