30a2b128c3647f7653452a5ff93c6351844b3500
Diogo Fiuza <difipalm@gmail.com>
2021-08-19 20:35:37 -0300

    Exercício 3

--- Fundamentos/bloco_10/dia _2/tests/exercise2.spec.js
+++ Fundamentos/bloco_10/dia _2/tests/exercise2.spec.js
@@ -2,19 +2,20 @@ const { it, expect } = require('@jest/globals');
 const { getUserName, users } = require('../src/exercise2');
 
 describe('Valida o resolve da função getUserName()', () => {
-  it('Resolve', () => (
-    getUserName(1).then((user) => {
+  it('Resolve',  async () => (
+    await getUserName(1).then((user) => {
       expect(user).toEqual('Mark')
     })
   ))
 })
 
 describe('Valida o reject da função getUserName()', () => {
-  it('Reject', () => {
+  it('Reject', async () => {
     expect.assertions(1);
-    return getUserName(3).catch((error) => (
-      expect(error.message).toMatch(`User with 3 not found.`)
-    ))
+  try {
+    await getUserName(3)
+  } catch (error) {
+     expect(error.message).toEqual(`User with 3 not found.`)
+  }
   })
 })
-
