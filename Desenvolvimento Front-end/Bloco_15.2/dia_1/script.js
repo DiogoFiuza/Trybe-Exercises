2b5ddb1f7277286d34aaaa2b3d8271254fa2ee24
Diogo Fiuza <difipalm@gmail.com>
2021-10-07 19:03:54 -0300

    .

--- Desenvolvimento Front-end/Bloco_15.2/dia_1/script.js
+++ Desenvolvimento Front-end/Bloco_15.2/dia_1/script.js
@@ -3,6 +3,7 @@ const ESTADO_INICIAL = {
   index: 0,
 };
 
+// Usar essa função
 function criarCor() {
   const oneChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
   let cor = '#';
