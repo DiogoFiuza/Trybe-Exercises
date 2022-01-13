3d898a29a222347907eacb42aae0a8775aa51c3c
DiogoFiuza <difipalm@gmail.com>
2022-01-13 12:01:36 -0300

    exercicio 2

--- Back End/Bloco_22/dia_4/index.js
+++ Back End/Bloco_22/dia_4/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
+
 const reqHandler = (_req, res) => {
   res.status(200).json({ message: 'pong' });
 }
@@ -10,4 +14,9 @@ app.get('/ping', reqHandler );
 
 app.listen(3001, () => {
   console.log('Escutando porta 3001');
+})
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.json({ "name": ` Hello ${name}`});
 })
